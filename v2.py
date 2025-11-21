425482452ba1dade57af5ab3c136aca342851c3e
Aryan Chavan <thearyanchavan30@gmail.com>
2025-11-21 06:42:03 +0530

    Commit 21/11/2025

--- v2.py
+++ v2.py
@@ -50,6 +50,27 @@ def estimate_loss():
     model.train()
     return out
 
+
+class Head(nn.Module):
+    """ one head of self-attention """
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))    
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,C)
+        q = self.query(x) # (B,T,C)
+        wei = q @ k.transpose(-2,-1) * C**-0.5 # (B,T,T)
+        wei = F.softmax(wei, dim=-1) # (B,T,T)
+        v = self.value(x) # (B,T,C)
+        out = wei @ v # (B,T,C)
+        return out
+
+
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
