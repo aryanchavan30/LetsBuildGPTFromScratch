3755572501574a43a614e66c4f51729e00591504
Aryan Chavan <AC12860@solargroup.com>
2025-11-21 13:01:43 +0530

    A run ready transformers

--- v3.py
+++ v3.py
@@ -3,14 +3,18 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 
-batch_size = 32
-block_size = 8
+batch_size = 64 # how many independent sequences will we process in paralle ?
+block_size = 256 # what is max context length for prediction
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"I'm using {device}")
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 torch.manual_seed(1473)
 
 with open('Solar_Industries_India_Comprehensive_Analysis.txt','r',encoding='utf-8') as f:
@@ -58,7 +62,8 @@ class Head(nn.Module):
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))    
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))  
+        self.dropout = nn.Dropout(dropout)  
 
     def forward(self, x):
         B,T,C = x.shape
@@ -66,6 +71,7 @@ class Head(nn.Module):
         q = self.query(x) # (B,T,C)
         wei = q @ k.transpose(-2,-1) * C**-0.5 # (B,T,T)
         wei = F.softmax(wei, dim=-1) # (B,T,T)
+        wei = self.dropout(wei)
         v = self.value(x) # (B,T,C)
         out = wei @ v # (B,T,C)
         return out
@@ -78,10 +84,11 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.proj(out)
+        out = self.dropout(self.proj(out))
         return out
 
 
@@ -93,6 +100,7 @@ class FeedForward(nn.Module):
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
             nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
         )
     
     def forward(self, x):
@@ -108,10 +116,12 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
     
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
         return x
 
 class BigramLanguageModel(nn.Module):
@@ -119,11 +129,8 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.positonal_embedding_table = nn.Embedding(block_size, n_embd)
-        self.block = nn.Sequential(
-            Block(n_embd, n_head = 4),
-            Block(n_embd, n_head = 4),
-            Block(n_embd, n_head = 4),
-        )
+        self.block = nn.Sequential(*[Block(n_embd , n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm 
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -171,4 +178,4 @@ for iter in range(max_iters):
     optimizer.step()
 
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(model.generate(context, max_new_tokens=100)[0].tolist()))
+print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
